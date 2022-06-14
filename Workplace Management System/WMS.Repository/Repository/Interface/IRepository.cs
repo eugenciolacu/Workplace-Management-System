2c580304079fcbded8646303f0fe240f98cb034c
Eugeniu Ciolacu <eugenciolacu@gmail.com>
2022-06-14 11:28:04 +0300

    2022.06.14 1st commit. Added draft Repository

---
+++ Workplace Management System/WMS.Repository/Repository/Interface/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMS.Data.Entity;
+
+namespace WMS.Repository.Repository.Interface
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        IEnumerable<T> GetAll();
+        T Get(Guid id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Remove(T entity);
+        void SaveChanges();
+    }
+}
