01c8d6ba74d30397c0616d518cb909843ef98e7c
Eugeniu Ciolacu <eugenciolacu@gmail.com>
2022-06-14 12:16:43 +0300

    2022.06.14 2nd commit. Added draft EmployeeService

---
+++ Workplace Management System/WMS.Service/Implementation/EmployeeService.cs
@@ -0,0 +1,43 @@
+﻿using WMS.Data.Entity.Core;
+using WMS.Repository.Repository.Interface;
+using WMS.Service.Interface;
+
+namespace WMS.Service.Implementation
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private IRepository<Employee> _employeeRepository;
+
+        public EmployeeService(IRepository<Employee> employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public IEnumerable<Employee> GetEmployees()
+        {
+            return _employeeRepository.GetAll();
+        }
+
+        public Employee GetEmployee(Guid id)
+        {
+            return _employeeRepository.Get(id);
+        }
+
+        public void InsertEmployee(Employee employee)
+        {
+            _employeeRepository.Insert(employee);
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            _employeeRepository.Update(employee);
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            Employee employee = _employeeRepository.Get(id);
+            _employeeRepository.Remove(employee);
+            _employeeRepository.SaveChanges();
+        }
+    }
+}
---
+++ Workplace Management System/WMS.Service/Interface/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using WMS.Data.Entity.Core;
+
+namespace WMS.Service.Interface
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetEmployees();
+        Employee GetEmployee(Guid id);
+        void InsertEmployee(Employee employee);
+        void UpdateEmployee(Employee employee);
+        void DeleteEmployee(Guid id);
+    }
+}
